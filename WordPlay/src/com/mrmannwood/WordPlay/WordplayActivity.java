6e49760f8437e6ce4bc6c20cf2a8d688f8d4a6cd
Marshall Mann-Wood <marshallmannwood@gmail.com>
2014-03-22 12:32:45 -0500

    created project in intellij

---
+++ WordPlay/src/com/mrmannwood/WordPlay/WordplayActivity.java
@@ -0,0 +1,15 @@
+package com.mrmannwood.WordPlay;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WordplayActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wordplay_activity);
+    }
+}
